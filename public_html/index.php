5d6b78d5d3324644fcbf789433a5b7022b9ed04e
yushghimire <yushghimire@gmail.com>
2018-02-10 08:07:27 +0545

    fixed the layout of the new design

--- public_html/index.php
+++ public_html/index.php
@@ -52,8 +52,8 @@
 		</div>
 	</div>
 	<!-- Full News Modal End -->
-	<div class="body">
-		<div id="mainTopPart"><!--top part enclosed in this div-->
+	<div class="site-wrapper">
+		<div class="header-wrapper"><!--top part enclosed in this div-->
 
 			<!-- Navbar moved up to the stars -->
 
@@ -102,129 +102,117 @@
 				<div class="hamburger"></div>
 				<div class="hamburger"></div>
 			</div>
+
 			<div class="ribbon"></div>
-			
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-md-6">
-						<div id="header">
-							<div id="myCarousel" class="carousel slide" data-ride="carousel">
-								<!-- Indicators -->
-								<ol class="carousel-indicators">
-									<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-									<li data-target="#myCarousel" data-slide-to="1"></li>
-									<li data-target="#myCarousel" data-slide-to="2"></li>
-								</ol>
+	
+		</div>
+		<div class="body-wrapper">
+					
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="carousel-wrapper">
+						<div id="myCarousel" class="carousel slide" data-ride="carousel">
+							<!-- Indicators -->
+							<ol class="carousel-indicators">
+								<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+								<li data-target="#myCarousel" data-slide-to="1"></li>
+								<li data-target="#myCarousel" data-slide-to="2"></li>
+							</ol>
 
-								<!-- Wrapper for slides -->
-								<div class="carousel-inner">
+							<!-- Wrapper for slides -->
+							<div class="carousel-inner">
 
-									<div class="item active carHeight">
-										
-										<img src="img/home/EA-Temp.jpg" class="cover" alt="Image" style="height: 50vh; width:100%; object-fit: cover;">
-										<!-- <div class="carousel-caption">
-											<div id="Main">
-												<div class="contentFont">
-													<center>
-														"Organic Essense of Himalaya"
-													</center>
-												</div>
+								<div class="item active carHeight">
+									
+									<img src="img/home/EA-Temp.jpg" class="cover" alt="Image" style="height: 50vh; width:100%; object-fit: cover;">
+									<!-- <div class="carousel-caption">
+										<div id="Main">
+											<div class="contentFont">
+												<center>
+													"Organic Essense of Himalaya"
+												</center>
 											</div>
-											<div id="Sub">
-												<div class="titleFont">
-													Everest Aroma
-												</div>
+										</div>
+										<div id="Sub">
+											<div class="titleFont">
+												Everest Aroma
 											</div>
-										</div> -->
-									</div>
-
-									<div class="item carHeight">
-										<img src="img/home/slider_1.jpg" class="cover" alt="Image" style="height: 50vh; width:100%; object-fit: cover;">
-										<!-- <div class="carousel-caption">
-											<div id="Main">
-												<div class="contentFont">
-													<center>
-														"Organic Essense of Himalaya"
-													</center>
-												</div>
-											</div>
-											<div id="Sub">
-												<div class="titleFont">
-													Everest Aroma
-												</div>
-											</div>
-										</div> -->
-									</div>
-
-									<div class="item carHeight">
-										<img src="img/home/slider_2.jpg" class="cover" alt="Image" style="height: 50vh; width:100%; object-fit: cover;">
-										<!-- <div class="carousel-caption">
-											<div id="Main">
-												<div class="contentFont">
-													<center>
-														"Organic Essense of Himalaya"
-													</center>
-												</div>
-											</div>
-											<div id="Sub">
-												<div class="titleFont">
-													Everest Aroma
-												</div>
-											</div>
-										</div> -->
-									</div>
-
+										</div>
+									</div> -->
 								</div>
 
-								<!-- Left and right controls -->
-								<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-									<span class="glyphicon glyphicon-chevron-left"></span>
-									<span class="sr-only">Previous</span>
-								</a>
-								<a class="right carousel-control" href="#myCarousel" data-slide="next">
-									<span class="glyphicon glyphicon-chevron-right"></span>
-									<span class="sr-only">Next</span>
-								</a>
+								<div class="item carHeight">
+									<img src="img/home/slider_1.jpg" class="cover" alt="Image" style="height: 50vh; width:100%; object-fit: cover;">
+									<!-- <div class="carousel-caption">
+										<div id="Main">
+											<div class="contentFont">
+												<center>
+													"Organic Essense of Himalaya"
+												</center>
+											</div>
+										</div>
+										<div id="Sub">
+											<div class="titleFont">
+												Everest Aroma
+											</div>
+										</div>
+									</div> -->
+								</div>
+
+								<div class="item carHeight">
+									<img src="img/home/slider_2.jpg" class="cover" alt="Image" style="height: 50vh; width:100%; object-fit: cover;">
+									<!-- <div class="carousel-caption">
+										<div id="Main">
+											<div class="contentFont">
+												<center>
+													"Organic Essense of Himalaya"
+												</center>
+											</div>
+										</div>
+										<div id="Sub">
+											<div class="titleFont">
+												Everest Aroma
+											</div>
+										</div>
+									</div> -->
+								</div>
 
 							</div>
-						</div><!--header-->
-						<!--danny part ends-->
-					</div>
-					<div class="col-md-6">
-						
-					</div>
-				</div>
-			
-			<!--jigree part starts-->
 
-			<!--jigree part end-->
-			<!-- <div id="aboveNavbarRight"> -->
-				<!-- Removed Logo  ~Godey 04/26-->
-				<!-- <img src="img/home/logo.jpg" class="img-responsive" id="eaLogo" alt="Photo Not Avaiable"> -->
-				<!-- <div class="container-fluid"  id="aboveNavbar"> -->
-					<!-- <div class="row"> -->
-						<!-- <div class="col-xs-6 imag">Assigning colum width -->
-						<!-- 	<a href="http://www.euroleaf.org/"><img src="img/home/EU-Organic.jpg" class="certificationImg"></a>
+							<!-- Left and right controls -->
+							<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+								<span class="glyphicon glyphicon-chevron-left"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#myCarousel" data-slide="next">
+								<span class="glyphicon glyphicon-chevron-right"></span>
+								<span class="sr-only">Next</span>
+							</a>
+
 						</div>
+					</div><!--header-->
+					<!--danny part ends-->
+				</div>
+				<div class="col-md-6">
+					<div id="aboveNavbarRight">
+						<div id="aboveNavbar">
+							<div class="row">
+								<div class="col-xs-6">
+									<a href="http://www.euroleaf.org/"><img src="img/home/EU-Organic.jpg" class="certificationImg"></a>
+								</div>
 
-						<div class="col-xs-6 imag">
-							<a href="https://www.ams.usda.gov/services/organic-certification"><img src="img/home/USDA.png" class="certificationImg"></a>
-						</div>		
-					</div>
-					<div class="row languageChoose">
-						<div class="col-xs-8" style="color: white; text-align: center;"><div class="contentFont">Choose Language</div></div>
-						<div class="col-xs-4">
-							<a href="index_fr.html">
-								<img src="img/home/france.png" class="button1">
-							</a>
-							<a href="index.html">
-								<img src="img/home/england.png" class="button2">
-							</a>
+								<div class="col-xs-6">
+									<a href="https://www.ams.usda.gov/services/organic-certification"><img src="img/home/USDA.png" class="certificationImg"></a>
+								</div>		
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div  class="container-fluid" id="quote" style="display: none;">
+			
+		
+			<!-- <div  class="container-fluid" id="quote" style="display: none;">
 				<div class="row">
 					<div class="col-xs-12">
 						<div id="Main">
@@ -245,14 +233,11 @@
 						</div>
 					</div>
 				</div>	
-			</div>	 -->	
-
-			
-		</div>
+			</div>		 -->
 		<!--top part enclosed div closes-->   
 
 		<!-- NEWS SECTION START -->
-		<div class="middle">    
+		<div class="row middle">    
 			<div class="row row-eq-height">
 				<div class="col-lg-4" >
 					<div id="news">
@@ -294,14 +279,6 @@
 
 				<div class="col-lg-8 introductionEverest">
 
-					<!-- for video -->
-					<div class="row" >
-						<div class="colInside">
-							<iframe width="100%" height="100%" src="https://www.youtube.com/embed/Vv54Ka6GG9A" frameborder="0" gesture="media" allowfullscreen></iframe>
-
-						</div>
-					</div>
-
 					<!--Intro of EA-->
 					<div class="row intro-wrapper" >
 						<div class="col introduction" onclick="myhref('redirectionofAboutus.php#WhyEverestAroma');">		    					
@@ -321,17 +298,21 @@
 							</div>
 						</div>
 					</div>
-				
+				</div>
+			</div>
+		</div>
 
-					<!--Kabir Part Starts-->
-					<div class="productContainer">
-							<div class="container-fluid">
-								<div class="row">
-									<br>
-									<div class="col-xs-12">
-										<div class="row">	
-											<div class="col-sm-6">			
-												<div class="row">				
+		<div class="row">
+				<div class="col-lg-4">
+				<!-- for video -->
+					<div class="colInside">
+						<iframe width="100%" height="100%" src="https://www.youtube.com/embed/Vv54Ka6GG9A" frameborder="0" gesture="media" allowfullscreen></iframe>
+					</div>
+				</div>
+				<div class="col-lg-8">
+						<!--Kabir Part Starts-->
+						<div class="productContainer">
+								<h2>Product</h2>		
 												<?php
 
 												include '../eaPortions/dbConnect.php';
@@ -345,13 +326,9 @@
 											                                  // output data of each row
 
 													while($row = mysqli_fetch_assoc($result)) {
-														 if($count == 2){
-														 	echo '</div></div> <div class="col-sm-6"><div class="row">';
-														 	$count=0;
-														 }
 														echo ' 
 																		 
-																			<div class="col-xs-6">
+																			<div class="col-lg-3 col-md-6 col-xs-12">
 																					<div class="product">
 																						<center>';
 																							echo '<a href="productsDescription.php?viewProduct'.$row["productID"].'=' .$row["productName"]. '">';
@@ -375,23 +352,20 @@
 												?>
 												</div>
 											</div>
-									</div>
-								</div>
-							</div>
-						</div>
 					</div>
 					<!--Kabir part ends-->
 				</div>
-			</div>
-		</div>		
+				
+		</div>	
+	</div>	
 <!-- Bhuwan Part Ends-->
-
-
-<?php 
-include 'footer.php';
-?>
-
-</div>
+		</div>
+		<div class="footer-wrapper">
+			<?php 
+			include 'footer.php';
+			?>
+		</div>
+	</div>
 
 <!-- Script of News Modal -->
 <script>
