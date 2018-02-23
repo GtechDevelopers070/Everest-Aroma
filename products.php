e507d67ae412aa4e155b9b82dcca702af028aaaa
ishanliv99 <ishanliv99@gmail.com>
2018-02-23 21:13:21 +0545

    Product page start, no css

---
+++ products.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Product</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="public_html/css/productsStylecss.css" />
+</head>
+
+<body>
+  <div class="main-outer-container">
+    <div class="main-inner-container">
+      <div class="upper-container">
+        <div class="upper-left-container">
+          <img src="./public_html/img/products/citronellaMain.png" alt="citronella">
+        </div>
+        <div class="upper-right-container">
+          <h1>Mentha</h1>
+          <ul>
+            <li>Product</li>
+            <li>Product</li>
+            <li>Product</li>
+            <li>Product</li>
+            <li>Product</li>
+            <li>Product</li>
+            <li>Product</li>
+            <li>Product</li>
+            <li>Product</li>
+            <li>Product</li>
+            <li>Product</li>
+            <li>Product</li>
+          </ul>
+        </div>
+      </div>
+      <div class="lower-container">
+        <div class="lower-left-container">
+          <h2>Description</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dolor, fuga quos facere, totam repellat vitae laboriosam tempora quibusdam similique beatae quas nisi labore ipsam ducimus qui aut iure quia.</p>
+        </div>
+        <div class="lower-right-container">
+          <h2>Physical and Chemical Properties</h2>
+          <ul>
+            <li>State</li>
+            <li>State</li>
+            <li>State</li>
+            <li>State</li>
+            <li>State</li>
+            <li>State</li>
+            <li>State</li>
+            <li>State</li>
+            <li>State</li>
+            <li>State</li>
+            <li>State</li>
+            <li>State</li>
+            <li>State</li>
+            <li>State</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
