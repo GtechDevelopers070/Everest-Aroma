331cb5369b110e81b1adc4f347e53f99b735195c
Kripesh Godey <kripsdhakal@gmail.com>
2018-03-19 11:44:14 +0545

    About Us updated according to new design

--- public_html/aboutUs.php
+++ public_html/aboutUs.php
@@ -23,143 +23,135 @@
             window.location.href = web;}
         </script>
         <!-- head close and body open in navbar.html -->
-        <div id="aboutUsInfo">  
-            <div class="container-fluid">
-                <div class="row equal">
-
-                    <!-- <a href="redirectionofAboutus.php#History"> -->
-                    <div class="col-xs-12 col-md-8 writtenBlocks" onclick="myhref('redirectionofAboutus.php#History');">
-                        <div class="bigBlockAbout">
-                            <h2 class="aboutTitle"> Our History</h2>
-                            <p>Everest Aroma was founded in 2010 by the Baral Family. The generations that followed continued with the traditional business of manufacturing herbal drugs. But now the business has been taken towards a new light by Mr. Sandeep Baral, who after completing his academic degree, ushered the new generation.</p>
-                        </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-8">
-
-                            </div>
-                            <div class="col-md-4">
-                                <span class="readMore">
-                                    READ MORE
-                                </span>
-                            </div>
-                        </div>
-                        <br>   
-                    </div>
-                    <!-- </a> -->
-                    <div class="col-xs-12 col-sm-6 col-md-4 blank">
-                        <br><br>
-                        <blockquote >
-                            We process herbs that are grown organically, or harvested wild in a sustainable manner in the Himalayas employing steam distillation for extracting our essential oils and hydrosols in order to preserve their natural properties providing immaculate product and their natural benefit to our buyers.
-                        </blockquote>
-                    </div>
-                    
-
-                    <div class="col-xs-12 col-sm-6 col-md-4 blank">
-                        <br><br>
-                        <blockquote >
-                            In addition to having certified organic essential oils, we also ­­have ethical business practices focusing on sustainability, women empowerment and social development. We provide employment opportunities and directly support families of the marginalized Musahar community empowering them and promoting their social and economic standards.
-                            <!-- <cite>Chairman</cite> -->
-                        </blockquote>
-                    </div>
-
-                    <!-- <a href="redirectionofAboutus.php#Portfolio"> -->
-                    <div class="col-xs-12 col-md-8 writtenBlocks" onclick="myhref('redirectionofAboutus.php#Portfolio');">
-                        <div class="bigBlockWork">
-                            <h2 class="aboutTitle"> Our Works</h2>
-                            <p> At current we are farming in Chisapani, Nawalparasi district in Chisapani Community forest, Sindhupalchowk and Rammechhap District which was badly hit by earthquake in April by focusing on development of local economy and sustainability of community forest.</p>
-                        </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-8">
-
-                            </div>
-                            <div class="col-md-4">
-                                <span class="readMore">
-                                    READ MORE
-                                </span>
-                            </div>
-                        </div>
-                        <br>  
-                    </div>
-                    <!-- </a> -->
-                    
-                </div>  
-            </div><!--end of container-->
-        </div>
         <div class="aboutUsHolder">
             <div class="container">
-                <div class="aboutUsWriting">
-                    <h2 class="aboutUsHeading"><div class="titleFont"> About Us </div></h2>
-                    <p class="aboutCompany"> <div class="contentFont"> In this company we manufacture essential oils made from wild and organically 
-                        grown spices and herbs. We cultivate these herbs in Chisapani community forest at Nawalparasi, Nepal 
-                        following the EU and USDA standards. These herbs are grown and gathered with the greatest care maintaining
-                        the biodiversity of the ecosystem throughout the process. Our qualified harvesters collect herbs from
-                        different collection centers we have set up all over Nepal. Furthermore, state of the art technology is
-                        used in all our field distillation units in Sindhupalchowk and Rammechhap and steam distillation units in 
-                        Chispani. All this is carried out in low heat and pressure to ensure the superior quality of our product. 
-                        The distilled product is then tested and packaged fittingly maintaining the essence of the oils which we 
-                        distribute within the country and also to the international markets including Europe and the United States.</div></p>
-                    </div><!--end of aboutUsWrinting-->
+                <div class="row">
+                    <div class="col-xs-12">
+                        
+                        <div class="aboutUsWriting">
+                            <h2 class="aboutUsHeading"><div class="titleFont"> About Us </div></h2>
+                            <p class="aboutCompany"> <div class="contentFont"> In this company we manufacture essential oils made from wild and organically 
+                                grown spices and herbs. We cultivate these herbs in Chisapani community forest at Nawalparasi, Nepal 
+                                following the EU and USDA standards. These herbs are grown and gathered with the greatest care maintaining
+                                the biodiversity of the ecosystem throughout the process. Our qualified harvesters collect herbs from
+                                different collection centers we have set up all over Nepal. Furthermore, state of the art technology is
+                                used in all our field distillation units in Sindhupalchowk and Rammechhap and steam distillation units in 
+                                Chispani. All this is carried out in low heat and pressure to ensure the superior quality of our product. 
+                                The distilled product is then tested and packaged fittingly maintaining the essence of the oils which we 
+                                distribute within the country and also to the international markets including Europe and the United States.</div></p>
+                            </div><!--end of aboutUsWrinting-->
 
 
-                    <div class="connectionBox"></div>
-                    <div class="ourProcess">
-                        <table>
+                            <div class="connectionBox"></div>
+                            <div class="ourProcess">
+                                <table>
 
-                            <tr>
-                                <td>
-                                    <div  class="processCircle">
-                                        <div class="processCircleDiv">
-                                            <p class="specificWriting">Organic herbs are grown, gathered and dried with proper care in local community forests</p>
-                                            <div class="processStep"><h3><div class="titleFont">Cultivation</div></h3></div>
-                                        </div>	
-                                    </div>
-                                </td><!--end of col-md-6-->
+                                    <tr>
+                                        <td>
+                                            <div  class="processCircle">
+                                                <div class="processCircleDiv">
+                                                    <p class="specificWriting">Organic herbs are grown, gathered and dried with proper care in local community forests</p>
+                                                    <div class="processStep"><h3><div class="titleFont">Cultivation</div></h3></div>
+                                                </div>  
+                                            </div>
+                                        </td><!--end of col-md-6-->
 
-                                <td>
-                                    <div  class="processCircle">
-                                        <div class="processCircleDiv">
-                                            <p class="specificWriting"> Collected by qualified local harvesters emphasizing on environmental sustainability </p>
-                                            <div class="processStep"><h3 ><div class="titleFont">Processing</div></h3></div>
-                                        </div>	
-                                    </div>
+                                        <td>
+                                            <div  class="processCircle">
+                                                <div class="processCircleDiv">
+                                                    <p class="specificWriting"> Collected by qualified local harvesters emphasizing on environmental sustainability </p>
+                                                    <div class="processStep"><h3 ><div class="titleFont">Processing</div></h3></div>
+                                                </div>  
+                                            </div>
 
-                                </td><!--end of col-md-6-->
+                                        </td><!--end of col-md-6-->
 
-                                <td>
-                                    <div  class="processCircle">
-                                        <div class="processCircleDiv">
-                                            <p class="specificWriting"> Distillation units with the state of the art technology are used to produce oil of superior quality</p>
-                                            <div class="processStep"><h3 ><div class="titleFont">Collection</div></h3></div>
-                                        </div>
-                                    </div>
-                                </td><!--end of col-md-6-->
+                                        <td>
+                                            <div  class="processCircle">
+                                                <div class="processCircleDiv">
+                                                    <p class="specificWriting"> Distillation units with the state of the art technology are used to produce oil of superior quality</p>
+                                                    <div class="processStep"><h3 ><div class="titleFont">Collection</div></h3></div>
+                                                </div>
+                                            </div>
+                                        </td><!--end of col-md-6-->
 
-                                <td>
-                                    <div  class="processCircle">
-                                        <div class="processCircleDiv">
-                                            <p class="specificWriting"> Properly packaged to maintain the essence of the oils for forever, available in volumes from 100ml</p>
-                                            <div class="processStep"><h3 ><div class="titleFont">Packaging</div></h3></div>
-                                        </div>
-                                    </div>
-                                </td><!--end of col-md-6-->
+                                        <td>
+                                            <div  class="processCircle">
+                                                <div class="processCircleDiv">
+                                                    <p class="specificWriting"> Properly packaged to maintain the essence of the oils for forever, available in volumes from 100ml</p>
+                                                    <div class="processStep"><h3 ><div class="titleFont">Packaging</div></h3></div>
+                                                </div>
+                                            </div>
+                                        </td><!--end of col-md-6-->
 
-                                <td>
-                                    <div  class="processCircle">
-                                        <div class="processCircleDiv">
-                                            <p class="specificWriting"> Delivered to international markets including Europe and the United States as per requirement</p>
-                                            <div class="processStep"><h3 ><div class="titleFont">Delivery</div></h3></div>
-                                        </div>
-                                    </div> 
-                                </td>
-                            </tr>
-                        </table><!--end of col-md-6-->
-                    </div>
+                                        <td>
+                                            <div  class="processCircle">
+                                                <div class="processCircleDiv">
+                                                    <p class="specificWriting"> Delivered to international markets including Europe and the United States as per requirement</p>
+                                                    <div class="processStep"><h3 ><div class="titleFont">Delivery</div></h3></div>
+                                                </div>
+                                            </div> 
+                                        </td>
+                                    </tr>
+                                </table><!--end of col-md-6-->
+                            </div>
 
 
-                </div><!--about us rectangle-->
+                        </div><!--about us rectangle-->
+                    </div>    
+                </div>
             </div><!--end of container-->
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="aboutUsWriting">
+                            <h2 class="aboutUsHeading"><div class="titleFont"> Our History </div></h2>
+                            <p class="aboutCompany"> <div class="contentFont"> Our company started small in 2010, when the Baral family developed their first organic herb farming and distillation operation. At the time, Sandeep Baral had already discovered the incredible power of essential oils, but because the quality of available oils varied so greatly, heâ€™d been unable to fully harness their potential. Baral familyâ€™s previous experience in the direct-selling industry enabled them to fulfill their vision and establish a business that would empower its members to share truly pure essential oils with the world. </div>
+                            </p>
+
+                            <p class="aboutCompany"> <div class="contentFont"> After establishing Everest Aroma in 2010, they developed more farmland in Kanchanpur and Bardiya, Nawalparasi and began cultivating Cintronella, Lemongrass, Chamomile, Eucalyptus, and many other herbs. Fueled by a growing demand for top-quality essential oils, Everest Aroma designed and built the technologically advanced essential oil distillery in Nepal.  </div>
+                            </p>
+
+                            <p class="aboutCompany"> <div class="contentFont"> In harmony with these efforts, Everest Aroma developed the community by introducing them to MAPs farming, uplifting their standards. Through Sandeep Baralâ€™s leadership, Everest Aroma has grown to become the world leader in essential oils. Based in Nepal, with offices and farms in various parts of the country, Everest Aroma stays true to Baralâ€™s original vision. With our steadfast commitment to essential oil purity, weâ€™ve inspired people everywhere to experience nature's gifts of wellness, as well as helping socially and economically marginalized communities, and to discover new opportunities for lifelong transformations.</div>
+                            </p>
+
+                        </div><!--end of aboutUsWrinting-->  
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="aboutUsWriting">
+                            <h2 class="aboutUsHeading"><div class="titleFont"> Our Portfolio </div></h2>
+                            <p class="aboutCompany"> <div class="contentFont"> Everest aroma is an established essential oil company in Nepal. Our traditional business model is based on the accomplishment of Nepalese herbal products in markets around the world. Based on the decision of the company to follow a sustainable business model, we have established this corporation in Chisapani community forest of the Nawalparasi district where we have also created several employment opportunities for people of marginalized Musahar community and empowered the women of Musahar community through our business. Our company also focuses on maintaining balance in the environment and bio diversity while following the norms and regulations of USDA, NOP and EU organic certification. In addition to business and social practices, our company has invested portions of its assets in the conservation of special and unique herbs found in the Himalayan region. </div>
+                            </p>
+
+                            <p class="aboutCompany"> <div class="contentFont">  <strong>Company Strategy</strong> </div>
+
+                            <ul class="contentFont">
+                                <li>
+                                    Purpose: To be a leader in the essential oil industry by providing pure and organic essential oils, enhanced services and relationship.
+                                </li>
+                                <li>
+                                    Vision: To provide quality services that exceeds the expectations of our esteemed customers concomitantly uplifting social and economic standards of marginalized communities.
+
+                                </li>
+                                <li>
+                                    Mission statement: To build long term relationships with our customers and clients and provide exceptional quality essential oils (pure and organic) by pursuing business through sustainable farming and advanced technology.
+
+                                </li>
+                                <li>
+                                    Core values: We believe in treating our customers with respect and faith. We grow through creativity, invention and innovation. We integrate honesty, integrity and business ethics into all aspects of our business. We have a belief of social development through our companyâ€™s actions and always focus on giving back to the communities.
+                                </li>                            
+                            </ul>
+                            </p>
+                            <p style="height: 30px;">
+                                
+                            </p>
+                        </div><!--end of aboutUsWrinting-->
+                    </div>
+                </div>
+            </div>
 
             <footer>
                 <?php
