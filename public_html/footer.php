9baa136b88e5eeaa47f1abcb6334739ed2b12956
Kripesh Dhakal <kripsdhakal@gmail.com>
2018-04-09 21:35:19 +0545

    Changed designed to developed, and updated footer links.

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,4 +1,6 @@
 <!-- bootstrap and footerStyle.css needed. -->
+
+
 <div id="footerContactUs">
     <div class="container-fluid">        
         <div class="row"> 
@@ -16,18 +18,25 @@
             </div>
            <div class="col-xs-4"> 
                 <center>
-                <div id="design"> Designed by <br><a class="footerNoUnderline" href="http://gtechdevelopers.com.np/" target="_blank" >Gtech Developers</a></div>
+                <div id="design"> Develpoed by <br><a class="footerNoUnderline" href="http://gtechdevelopers.com.np/" target="_blank" >Gtech Developers</a></div>
                    </center> 
             </div> <!-- for spacing -->
             <div class="col-xs-4" id="footerFollowUs">
                 <center>Follow Us <br>
-
-                <a href="https://www.facebook.com/" target="_blank"> <img class="img-responsive footerContactImg" src="img/home/fb.png" ></a>
-                <a href="https://www.twitter.com/" target="_blank"> <img class="img-responsive footerContactImg" src="img/home/twitter.png" ></a>
-                <a href="https://www.youtube.com/" target="_blank">  <img class="img-responsive footerContactImg" src="img/home/youtube.png" ></a>
+                
+                 <a href=" https://www.facebook.com/Everest-Aroma-1543878225890892/" target="_blank"> <img class="img-responsive footerContactImg" src="img/home/fb.png" ></a>
+                <a href="https://www.linkedin.com/in/sandeep-baral-a1389243/" target="_blank"> <img class="img-responsive footerContactImg" src="img/home/twitter.png" ></a>
+                <a href="https://www.youtube.com/channel/UCsDiVHgnH2l4yq5HuicYe8g" target="_blank">  <img class="img-responsive footerContactImg" src="img/home/youtube.png" ></a>
                </center>
-               </div>   
-        </div>   
+                     
+               </div> 
+            
+            
+        </div>
+               
+          
+
+            
     </div>
 </div>
         
